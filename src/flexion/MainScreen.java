aac7054e9d3999a89548d8646d35e7a001259b67
Jesus Gutierrez <gusj93@hotmail.com>
2016-01-21 00:52:10 -0600

    Implementacion de la vista DeformacionesInput

--- src/flexion/MainScreen.java
+++ src/flexion/MainScreen.java
@@ -380,7 +380,11 @@ public class MainScreen extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new DeformacionesInput(f).setVisible(true);
+            }
+        });
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
